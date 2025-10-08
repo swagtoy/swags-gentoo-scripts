d685560b67bc30abccda8cadbaba307ebfb82cd7
Swagtoy <me@ow.swag.toys>
2025-10-08 03:31:18 -0400

    Initial, cargolock2crates
    Signed-off-by: Swagtoy <me@ow.swag.toys>

---
+++ README.md
@@ -0,0 +1,6 @@
+# Swagtoy's Gentoo Scripts
+
+What it says on the box. 
+
+Most of these are Perl scripts or whatever language I feel fits, so
+make sure Perl is at least installed. Handle with care.
---
+++ cargolock2crates.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+# Licensed under the BSD-3 Clause license. See LICENSE.
+
+# A simple for generating the CRATES variable for ebuilds.
+#
+# It was made when I had some issues with pycargoebuild. That one does
+#   a lot of extra fancy stuff but all this really does is exactly
+#   what it says on the tin. If you have issues with pycargoebuilds
+#   crate generating ability this may come in handy. It won't be
+#   sufficient though, as you'll be responsible for getting at the
+#   licenses.
+
+# TODO make this function as a module too
+
+my $cargofile = "Cargo.lock";
+open my $fh, $cargofile or die "Couldn't open $cargofile: $!";
+
+my $output = "CRATES=\"\n";
+
+my $curr_name;
+while (my $line = <$fh>)
+{
+	my $res;
+	
+	if ($line =~ /name = "(.*?)"/)
+	{
+		$curr_name = $1;
+		next;
+	}
+	
+	if ($line =~ /version = "(.*?)"/)
+	{
+		my $version = $1;
+		
+		$output .= "\t" . $curr_name . '@' . "$version\n";
+	}
+}
+
+$output .= "\"";
+print $output . "\n";
+
+close $fh;
