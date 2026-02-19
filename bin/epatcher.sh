3e00df239b8c4bc8707c4a6f7e88e5483d5774b8
Swagtoy <me@ow.swag.toys>
2026-02-19 00:03:17 -0500

    epatcher: make it even scarier & probably more correct-ish-not-really
    I actually hate this script with all my heart but i wanted to take a
    better stab at it. It works for one ebuild. Haven't tested anything
    else. Probably don't want to. This thing has literal worms.

--- bin/epatcher.sh
+++ bin/epatcher.sh
@@ -6,26 +6,33 @@
 EBUILD=$1
 REPO_PATH=$2
 [[ -v 2 ]] || REPO_PATH=$(pwd)
+# lie to ebuild.sh
+shift 2
 
-# Yep.
-inherit() { :; }
-python_gen_cond_dep() { :; }
+ver_test() { :; }
 ver_cut() { :; }
-python_gen_any_dep() { :; }
+ver_rs() { :; }
+# is this morally correct
+PORTAGE_BIN_PATH=$(printf /usr/lib/portage/python*/)
+PORTAGE_PYM_PATH=$(printf /usr/lib/portage/python*/)
+PORTAGE_ECLASS_LOCATIONS=( "/var/db/repos/gentoo" )
+_IN_INSTALL_QA_CHECK=0
+source /usr/lib/portage/*/ebuild.sh
 
 die() { echo "$1" 1>&2; exit 1; }
 
-FILESDIR=$(dirname "$EBUILD")/files
 
 echo " --- Using $EBUILD..."
-source "$EBUILD"
+source "$EBUILD"  2>/dev/null
 
 [[ -v DESCRIPTION ]] || die "No description!"
 [[ -v PATCHES ]] || die "No patches to apply!"
 
 [[ "$REPO_PATH" = "$(pwd)" ]] || echo " --- Applying to specified directory: $REPO_PATH";
 
+my_filesdir=$(dirname "$EBUILD")/files
 for patch in "${PATCHES[@]}"; do
+	patch="$my_filesdir$patch"
 	read -N 1 -p "Apply \"$patch\"? " apply
 	case $apply in
 		[Yy]*) echo; patch < "$patch";;
