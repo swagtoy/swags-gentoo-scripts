92490206fe58e6087098c19ade2b2840dc14a9ad
Swagtoy <me@ow.swag.toys>
2026-02-18 23:18:40 -0500

    snagit: something for copying things over
    My previous workflow involved copying crap from my local repo to my
    gentoo ebuild and i had some scripts to help me with that (in fact
    reporoot.pl was precisely made to assist with that), but now i'm
    basically riding off of the fact that
    
    An example usage might be:
    
      ./snagit.sh -r /home/me/gentoo/ master..HEAD
    
    where master is not HEAD, a typical case where you have a PR/fork.
    
    There was some literal "files" option that i did not bother
    implementing anyway but that was dumb because i think i'd rather copy
    all the bits over, though I might implement that later.

---
+++ bin/snagit.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# Licensed under the BSD-3 Clause license. See LICENSE.
+
+# Script for copying ebuilds over from some git 
+
+stuff=()
+
+opt_repo=$(pwd)
+opt_files=0
+opt_stfu=0
+
+while getopts "r:si" opt; do case $opt in
+	r)  opt_repo=$OPTARG; ;;
+	i) opt_files=1 ;;
+	s)  opt_stfu=1 ;;
+	\?) echo "YOU DUMBASS!"; exit 1 ;;
+esac done
+shift $((OPTIND-1))
+
+opt_range=$1
+
+function prompt_stuff()
+{
+	echo "These 'things' will get snagged and dropped into $(pwd)."
+	echo
+	for thing in "${stuff[@]}"; do
+		echo -e "  \x1b[1m$thing\x1b[0m"
+	done
+	
+	if [[ $opt_stfu -eq 0 ]]; then
+		echo
+		printf "Do you consent? (y/n) "
+		read -n 1 ans
+		echo
+		[[ $ans == "y" ]]
+		return $?
+	fi
+	echo
+	return 0
+}
+
+for file in $(git --git-dir="${opt_repo}/.git/" log "${opt_range}" --name-only --format=); do
+	if [[ $opt_files -eq 1 ]]; then
+		echo ":/"
+	else
+		justdir=$(dirname "${file}")
+		# ignore duplicates
+		exists=0
+		for dir in "${stuff[@]}"; do
+			[[ "$dir" == "$justdir" ]] && exists=1 && break
+		done
+		[[ exists -eq 0 ]] && stuff+=( "${justdir}" )
+	fi
+done
+
+prompt_stuff || exit 1
+
+for file in "${stuff[*]}"; do
+	if [[ $opt_files -eq 0 ]]; then
+		mkdir -p "./$file"
+		echo "${opt_repo}$file" -\> "./$file"
+		cp -r "${opt_repo}$file" $(dirname "./$file")
+	fi
+done
